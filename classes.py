1b8c4a4e32ebdf059553cf5b1a05bbd4e4e03993
kaede <jzh4ng@berkeley.edu>
2022-12-05 23:01:09 -0800

    rewrote playlist class and start rewriting calc_playlist_duration method

--- classes.py
+++ classes.py
@@ -1,29 +1,175 @@
 from googleapiclient.discovery import build
-
+from utils import *
 
 
 class ProgramError(Exception):
     """Exception indicating an error in the program."""
 
-class Resource:
-    """All share same API key"""
-    #note count how many times the build gets called and how many times the close called
-    api_key = 'AIzaSyA_GtkfzWH22w_qtB9ACWpBeGsPMPhYgYk'
+class InputError(Exception):
+    """Exception indicating an error in the program."""
 
-    yt = build('youtube', 'v3', developerKey=api_key)
+
+#################
+# API Resources #
+#################
+
+class Resource:
+    """All Resources share same the API key"""
+    API_KEY = 'AIzaSyA_GtkfzWH22w_qtB9ACWpBeGsPMPhYgYk'
+
+    yt = build('youtube', 'v3', developerKey=API_KEY)
 
     def __init__(self, **kwargs):
+        """Create a default Resource object"""
         self.__dict__.update(kwargs)
+        self.resource = None
+
+    def end_service(self):
+        yt.close()
+
 
 class Channels(Resource):
 
     def __init__(self, **kwargs):
+        """Create a Channels Resource object"""
         super().__init__(**kwargs)
-
+        self.resource = self.channels_request()
+        
     def channels_request(self):
-        return Resource.yt.channels().list(**self.__dict__)
+        query = {key: value for (key, value) in self.__dict__.items() if key != 'resource'}
+        request = Resource.yt.channels().list(**query)
+        return request.execute()
 
-    """Class functions"""
+    def get_channel_name(self):
+        print(self.__dict__['forUsername'])
+
+    """class methods"""
+
+
+class PlaylistItems(Resource):
+
+    def __init__(self, **kwargs):
+        """Create a PlaylistItems Resource object"""
+        super().__init__(**kwargs)
+        self.resource = self.playlistItems_request()
+        self.video_ids = self.get_vid_id_list()
+        
+        #if(len(self.video_ids) == 0):
+        #    raise InputError("playlist cannot be empty")
+
+    def playlistItems_request(self):
+        query = {key: value for (key, value) in self.__dict__.items() if key != 'resource'}
+        request = Resource.yt.playlistItems().list(**query)
+        return request.execute()
+        
+    def get_vid_id(self, vid):
+        """get_vid_id takes in a dictionary and returns a string representing the video ID (same ID shown in yt url)"""
+        return vid['contentDetails']['videoId']
+
+    def get_vid_ids(self):
+        return [self.get_vid_id(vid) for vid in self.resource['items']]
+
+    def get_vid_id_list(self):
+        """Returns a timestamp representing the total length of a YouTube playlist."""
+        nextPageToken = None
+        original_object = self.__dict__
+        
+        lst = []
+
+        while True:
+
+            lst.extend(self.get_vid_ids())
+
+            try:
+                nextPageToken = self.resource['nextPageToken'] 
+            except:
+                nextPageToken = None
+
+            if not nextPageToken: #breaks if next page doesn't exist
+                break
+       
+            newObj = PlaylistItems(
+                part='contentDetails',
+                playlistId=original_object['playlistId'],
+                maxResults=50,
+                pageToken=nextPageToken)
+
+            self.__dict__.update(newObj.__dict__)
+
+        self.__dict__.update(original_object) #reset to original
+
+        return lst
+
+
+    def calc_playlist_duration(self):
+        """Returns a timestamp representing the total length of a YouTube playlist."""
+        #Need fix, video request to API cannot be larger than 50 videos at a time. Break the list into chunks?
+        total_seconds = 0    
+        print("DEBUG: ",len(self.video_ids))
+        vid_id_lst_str = ','.join(self.video_ids)
+
+        vid_resource = Videos(
+            part='contentDetails',
+            id=vid_id_lst_str)
+
+        total_seconds += total_seconds_iso(vid_resource.resource['items'])
+
+        return timestamp(total_seconds)
+
+    """
+    def find_playlist_owner(id):
+        Finds the owner of the playlist, returns a string, raises error if playlist is empty
+
+        Currently, a bug in the API exists such that a collaborative playlist doesn't differentiate
+        who added videos, the API will always return the playlist owner. Thus the else clause will not run
+
+        nextPageToken = None
+
+        pl_request = playlistItems_request(part='snippet', playlistId=id, maxResults=50, pageToken=nextPageToken)
+
+        pl_response = pl_request.execute()
+
+        lst = [pl_response['items'][n]['snippet'].get('channelTitle') for n in range(len(pl_response['items']))]
+
+        if(len(set(lst)) == 0):
+            raise ProgramError("Cannot have empty playlist")
+
+        elif(len(set(lst)) == 1):
+            return lst[0]
+
+        else:
+            return "Multiple authors"
+    """
+
+
+
+class Videos(Resource):
+
+    def __init__(self, **kwargs):
+        """Create a Channels Resource object"""
+        super().__init__(**kwargs)
+        self.resource = self.videos_request()
+       
+    def videos_request(self):
+        query = {key: value for (key, value) in self.__dict__.items() if key != 'resource'}
+        request = Resource.yt.videos().list(**query)
+        return request.execute()
+
+    def get_vid_length(vid):
+        #get_vid_length takes in a dictionary and returns a string representing the video playtime 
+        #unformated video length: PT##H##M##S (playtime x hours, x minutes, x seconds)
+        return vid['contentDetails']['duration']
+
+
+
+    
+
+
+
+
+
+
+
 
 """
 class PlaylistItems(Resource):
--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
+import isodate
+import re
+import time
+
 from googleapiclient.discovery import build
+from datetime import timedelta
+
 
 def timer():
 	start_time = time.time()
@@ -6,6 +12,48 @@ def timer():
 	result = (time.time() - start_time)
 	print(f"--- Time elapsed: {result} seconds ---")
 
+#calculates the total length in seconds using regular expressions, returns seconds as an int
+def total_seconds_re(vid_list):
+	hours_pattern = re.compile(r'(\d+)H')
+	minutes_pattern = re.compile(r'(\d+)M')
+	seconds_pattern = re.compile(r'(\d+)S')
+
+	pl_total_seconds = 0
+
+	for vid in vid_list:
+		duration = get_vid_length(vid)
+
+		hours = hours_pattern.search(duration)
+		minutes = minutes_pattern.search(duration)
+		seconds = seconds_pattern.search(duration)
+
+		hours = int(hours.group(1)) if hours else 0
+		minutes = int(minutes.group(1)) if minutes else 0
+		seconds = int(seconds.group(1)) if seconds else 0
+
+		video_seconds = timedelta(
+			hours=hours,
+			minutes=minutes,
+			seconds=seconds
+		).total_seconds()
+
+		pl_total_seconds += video_seconds
+
+	return int(pl_total_seconds)
+
+
+#calculates the total length in seconds using isodate utility functions, returns seconds as an int
+def total_seconds_iso(vid_list):
+	pl_total_seconds = 0
+	
+	for vid in vid_list:
+		iso_8601_duration = vid['contentDetails']['duration'] #get_vid_length(vid) 
+		dt = isodate.parse_duration(iso_8601_duration)
+		vid_total_seconds = dt.total_seconds() #video length in seconds (int)
+
+		pl_total_seconds += vid_total_seconds
+
+	return int(pl_total_seconds)
 
 def timestamp(total_seconds):
 	"""changes seconds integer to HH:MM:SS (returns a string)"""
@@ -28,29 +76,6 @@ def get_video_https_link(vid_id):
 def get_playlist_https_link(pl_id):
 	return "https://www.youtube.com/watch?v=" + pl_id
 
-################
-# API Requests #
-################
 
-api_key = 'AIzaSyA_GtkfzWH22w_qtB9ACWpBeGsPMPhYgYk'
-
-yt = build('youtube', 'v3', developerKey=api_key)
-
-def channels_request(**kwargs):
-	"""**kwargs are the attributes for list()"""
-	return yt.channels().list(**kwargs)
-
-
-def playlistItems_request(**kwargs):
-	"""**kwargs are the attributes for list()"""
-	return yt.playlistItems().list(**kwargs)
-
-
-def videos_request(**kwargs):
-	"""**kwargs are the attributes for list()"""
-	return yt.videos().list(**kwargs)
-
-
-yt.close()
 
 
